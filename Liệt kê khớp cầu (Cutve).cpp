77a9a46db90d38c43de569c3337fd62bbcec5cf1
djxone123456 <46621320+djxone123456@users.noreply.github.com>
2020-08-16 15:52:00 +0700

    Create Liệt kê khớp cầu (Cutve).cpp

---
+++ Liệt kê khớp cầu (Cutve).cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define MP make_pair
+#define PB push_back
+#define INF INT_MAX
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+using namespace std;
+
+typedef pair <int, int> pi;
+typedef long long ll;
+typedef unsigned long long ull;
+
+/// Neu ko nghi ra solution thi chi can bo 1 canh / dinh roi dem so TPLT (dfs / bfs).
+///Neu increase thi la cau / khop
+//////////////////////////////////
+///Y tuong bai nay la DFS de tim cac Low[]
+///Khop: Low[u] (new) = min(Low[u] (old) , Low[v])
+///Cau: Low[v] >= Num[v]
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    #ifdef djxone123456
+    freopen("debug.inp","r",stdin);
+    freopen("debug.out","w",stdout);
+    #endif
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
